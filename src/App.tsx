85e85f0a7b131b910b23dae990c8c49af5841e39
unknown <swine777@gmail.com>
2023-01-08 16:08:41 +0900

    분석환경일때 메인타이틀 변경되게

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import "./App.css"
 
 function App() {
   const params = new URLSearchParams(location.search);
-
+  if(params.get('pageId')==='AnalysisTool'){
+    let h3:any = document.getElementsByClassName('title')[0];
+    h3.innerText = '분석환경'
+  }
+  
   return (
     <div className="App">
       {params.get('pageId')==='AnalysisTool' ? <AnalysisTool/> : <Cooperation/> }
